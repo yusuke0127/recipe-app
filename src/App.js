10a4f72ea689d3071412ce76373659322b04c50a
yusuke ishida <ishidayusuke01@gmail.com>
2020-07-22 19:44:55 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = '81444b84';
+  const APP_KEY = '5be88a11b270fd7c07ff10a0564983f2';
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect( () => {
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input type="text" className="search-bar"/>
+        <button type="submit" className="search-button">Search
+        </button>
+      </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
-}
+};
 
 export default App;
